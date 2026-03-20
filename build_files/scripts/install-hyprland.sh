3fce97de23c647b21898309491a6b26c2cf29f3f
Zach Handley <zachhandley@gmail.com>
2026-03-20 14:02:20 -0700

    Swap wdisplays -> nwg-displays (I think this one will work)

--- build_files/scripts/install-hyprland.sh
+++ build_files/scripts/install-hyprland.sh
@@ -57,9 +57,7 @@ rm /tmp/catppuccin-cursors.zip
 # --- nwg-displays (Hyprland monitor config GUI, not in Fedora repos) ---
 dnf5 install -y gtk-layer-shell python3-gobject python3-i3ipc python3-build python3-installer python3-setuptools python3-wheel
 git clone --depth 1 https://github.com/nwg-piotr/nwg-displays.git /tmp/nwg-displays
-cd /tmp/nwg-displays && python3 -m build --wheel --no-isolation
-mkdir -p /usr/local/bin
-python3 -m installer --destdir=/ dist/*.whl
+cd /tmp/nwg-displays && ./install.sh
 cd / && rm -rf /tmp/nwg-displays
 
 # --- nwg-look (GTK settings editor for wlroots) ---
